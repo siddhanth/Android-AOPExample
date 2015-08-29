290ce8b4e8657e0374ea4f3a6ac2e535f98f8da9
Siddhanth Jain <siddhanth.iitb@gmail.com>
2015-08-30 02:33:28 +0530

    Adding code

--- app/src/main/java/org/android10/viewgroupperformance/activity/MainActivity.java
+++ app/src/main/java/org/android10/viewgroupperformance/activity/MainActivity.java
@@ -8,22 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.android10.gintonic.aspect.Constants;
 import org.android10.gintonic.aspect.TraceAspect;
 import org.android10.viewgroupperformance.R;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button button1, button2, button3, button4, button5, button6, button7, button8;
-    private Button buttonToggle;
     Context mContext;
     private AlertDialog dialog;
-
-    public static String OFF = "OFF";
-    public static String ON = "ON";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PrefUtils.setDebugPref(this, Constants.ON);
         TraceAspect.init(this);
         setContentView(R.layout.activity_main);
         mContext = this;
@@ -34,13 +32,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
      * Maps Graphical User Interface
      */
     private void mapGUI() {
-        buttonToggle = (Button) findViewById(R.id.button_toggle);
-        buttonToggle.setText(PrefUtils.getDebugPref(mContext));
-        if (buttonToggle.getText().toString().equals("ON")) {
-            buttonToggle.setBackgroundColor(getResources().getColor(R.color.green));
-        } else if (buttonToggle.getText().toString().equals("OFF")) {
-            buttonToggle.setBackgroundColor(getResources().getColor(R.color.red));
-        }
 
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -51,8 +42,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         button7 = (Button) findViewById(R.id.button7);
         button8 = (Button) findViewById(R.id.button8);
 
-//        buttonToggle.setOnClickListener(this);
-//        button1.setOneClickListener(this);
+        button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
@@ -63,36 +53,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
 
-    /*private View.OnClickListener btnRelativeLayoutOnClickListener = new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        openActivity(RelativeLayoutTestActivity.class);
-      }
-
-      private View.OnClickListener btnRelativeLayoutOnClickListener = new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              openActivity(RelativeLayoutTestActivity.class);
-          }
-      };
-
-      private View.OnClickListener btnLinearLayoutOnClickListener = new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              openActivity(LinearLayoutTestActivity.class);
-          }
-      };
-
-      private View.OnClickListener btnFrameLayoutOnClickListener = new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              openActivity(FrameLayoutTestActivity.class);
-          }
-      };
-
-      /**
-       * Open and activity
-       */
     private void openActivity(Class activityToOpen) {
         Intent intent = new Intent(this, activityToOpen);
         startActivity(intent);
@@ -133,18 +93,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void f123(View view) {
-
     }
 
-    public void annotate(View view) {
-        if (buttonToggle.getText().toString().equals(ON)) {
-            PrefUtils.setDebugPref(mContext, OFF);
-            buttonToggle.setText(OFF);
-            buttonToggle.setBackgroundColor(getResources().getColor(R.color.red));
-        } else if (buttonToggle.getText().toString().equals(OFF)) {
-            PrefUtils.setDebugPref(mContext, ON);
-            buttonToggle.setText(ON);
-            buttonToggle.setBackgroundColor(getResources().getColor(R.color.green));
-        }
-    }
 }
--- gintonic/src/main/java/org/android10/gintonic/aspect/FunctionStore.java
+++ gintonic/src/main/java/org/android10/gintonic/aspect/FunctionStore.java
@@ -1,9 +1,24 @@
 package org.android10.gintonic.aspect;
 
+import android.content.Context;
+
 import org.android10.gintonic.annotation.NoTrace;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by siddhanthjain on 29/08/15.
@@ -12,50 +27,136 @@ public class FunctionStore {
 
     JSONObject store;
     private static FunctionStore fsObj;
+    Context context;
 
     @NoTrace
-    private void load(){
+    private void load() {
         store = new JSONObject();
+        try {
+            FileInputStream fin = context
+                    .openFileInput(Constants.FunctionStore);
+            String storeData = getStringFromInputStream(fin);
+            store = new JSONObject(storeData);
+            fin.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @NoTrace
-    private FunctionStore() {
+    public String getStringFromInputStream(InputStream is) {
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @NoTrace
+    private FunctionStore(Context context) {
+        this.context = context;
         load();
     }
 
     @NoTrace
-    public static FunctionStore get() {
+    public void save() throws IOException {
+        FileOutputStream fOut = context.openFileOutput(
+                Constants.FunctionStore, Context.MODE_PRIVATE);
+        fOut.write(fsObj.toString().getBytes());
+        fOut.close();
+        uploadToServer();
+    }
+
+    @NoTrace
+    public static FunctionStore get(Context context) {
         if (fsObj == null) {
-            fsObj = new FunctionStore();
+            fsObj = new FunctionStore(context);
         }
         return fsObj;
     }
 
     @NoTrace
-    public void addMethod(String className, String functionName, String ...params) throws JSONException {
+    public void addMethod(String className, String functionName, String... params) throws
+            JSONException, IOException {
         if (!store.has(className)) {
             store.put(className, new JSONArray());
         }
+
+        String eventName = "";
+        if (params!=null && params.length>0){
+            eventName = params[0];
+        }
+
         JSONArray arr = (JSONArray) store.get(className);
-        arr.put(functionName);
+        boolean methodPresent = false;
+        for (int ix = 0; ix < arr.length(); ix++) {
+            if (arr.get(ix).equals(functionName)) {
+                methodPresent = true;
+            }
+        }
+        if (!methodPresent) {
+            JSONObject obj = new JSONObject();
+            obj.put(Constants.FUNCTION_NAME, functionName);
+            obj.put(Constants.EVENT_NAME, eventName);
+            arr.put(obj);
+            save();
+
+        }
     }
 
     @NoTrace
     public boolean checkIfMethodPresent(String classname, String functionName) throws
             JSONException {
-        if (store == null){
+        if (store == null) {
             load();
         }
-        boolean present  = false;
-        if(store.has(classname)){
+        boolean present = false;
+            if (store.has(classname)) {
             JSONArray arr = (JSONArray) store.get(classname);
-            for(int ix=0;ix<arr.length();ix++){
-                String curObj = arr.getString(ix);
-                if(curObj.equals(functionName)){
-                    present = true;
+            for (int ix = 0; ix < arr.length(); ix++) {
+                JSONObject jObj = arr.getJSONObject(ix);
+                if (jObj.getString(Constants.FUNCTION_NAME).equals(functionName)){
+                    present =  true;
                 }
             }
         }
         return present;
+    }
+
+    @NoTrace
+    public void uploadToServer() throws FileNotFoundException {
+        String url = Constants.SERVER_PATH;
+        FileInputStream fin = context
+                .openFileInput(Constants.FunctionStore);
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+            InputStreamEntity reqEntity = new InputStreamEntity(
+                    fin, -1);
+            reqEntity.setContentType("binary/octet-stream");
+            reqEntity.setChunked(true); // Send in multiple parts if needed
+            httppost.setEntity(reqEntity);
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (Exception e) {
+            // show error
+        }
     }
 }
