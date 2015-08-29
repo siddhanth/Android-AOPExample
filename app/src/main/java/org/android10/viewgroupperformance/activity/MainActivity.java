2390f5bb61d25dbf4feceb7619bb497f638b43db
Siddhanth Jain <siddhanth.iitb@gmail.com>
2015-08-29 17:55:10 +0530

    adding code for tracking

--- app/src/main/java/org/android10/viewgroupperformance/activity/MainActivity.java
+++ app/src/main/java/org/android10/viewgroupperformance/activity/MainActivity.java
@@ -3,63 +3,70 @@ package org.android10.viewgroupperformance.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.android10.gintonic.aspect.Constants;
 import org.android10.viewgroupperformance.R;
 
 public class MainActivity extends Activity {
 
-  private Button btnRelativeLayoutTest;
-  private Button btnLinearLayoutTest;
-  private Button btnFrameLayoutTest;
+    private Button btnRelativeLayoutTest;
+    private Button btnLinearLayoutTest;
+    private Button btnFrameLayoutTest;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    mapGUI();
-  }
-
-  /**
-   * Maps Graphical User Interface
-   */
-  private void mapGUI() {
-    this.btnRelativeLayoutTest = (Button) findViewById(R.id.btnRelativeLayout);
-    this.btnRelativeLayoutTest.setOnClickListener(btnRelativeLayoutOnClickListener);
-
-    this.btnLinearLayoutTest = (Button) findViewById(R.id.btnLinearLayout);
-    this.btnLinearLayoutTest.setOnClickListener(btnLinearLayoutOnClickListener);
-
-    this.btnFrameLayoutTest = (Button) findViewById(R.id.btnFrameLayout);
-    this.btnFrameLayoutTest.setOnClickListener(btnFrameLayoutOnClickListener);
-  }
-
-  private View.OnClickListener btnRelativeLayoutOnClickListener = new View.OnClickListener() {
     @Override
-    public void onClick(View v) {
-      openActivity(RelativeLayoutTestActivity.class);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapGUI();
     }
-  };
 
-  private View.OnClickListener btnLinearLayoutOnClickListener = new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-      openActivity(LinearLayoutTestActivity.class);
+    /**
+     * Maps Graphical User Interface
+     */
+    private void mapGUI() {
+        this.btnRelativeLayoutTest = (Button) findViewById(R.id.btnRelativeLayout);
+        this.btnRelativeLayoutTest.setOnClickListener(btnRelativeLayoutOnClickListener);
+
+        this.btnLinearLayoutTest = (Button) findViewById(R.id.btnLinearLayout);
+        this.btnLinearLayoutTest.setOnClickListener(btnLinearLayoutOnClickListener);
+
+        this.btnFrameLayoutTest = (Button) findViewById(R.id.btnFrameLayout);
+        this.btnFrameLayoutTest.setOnClickListener(btnFrameLayoutOnClickListener);
     }
-  };
 
-  private View.OnClickListener btnFrameLayoutOnClickListener = new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-      openActivity(FrameLayoutTestActivity.class);
+    private View.OnClickListener btnRelativeLayoutOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity(RelativeLayoutTestActivity.class);
+        }
+    };
+
+    private View.OnClickListener btnLinearLayoutOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity(LinearLayoutTestActivity.class);
+        }
+    };
+
+    private View.OnClickListener btnFrameLayoutOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openActivity(FrameLayoutTestActivity.class);
+        }
+    };
+
+    /**
+     * Open and activity
+     */
+    private void openActivity(Class activityToOpen) {
+        Intent intent = new Intent(this, activityToOpen);
+        startActivity(intent);
     }
-  };
 
-  /**
-   * Open and activity
-   */
-  private void openActivity(Class activityToOpen) {
-    Intent intent = new Intent(this, activityToOpen);
-    startActivity(intent);
-  }
+    public void logFunction(View view) {
+        Log.d(Constants.TAG, "function called logFunction");
+    }
 }
--- gintonic/src/main/java/org/android10/gintonic/aspect/Constants.java
+++ gintonic/src/main/java/org/android10/gintonic/aspect/Constants.java
@@ -4,5 +4,5 @@ package org.android10.gintonic.aspect;
  * Created by siddhanthjain on 29/08/15.
  */
 public class Constants {
-    public final static String TAG= "DynamicTrackign";
+    public final static String TAG= "DynamicTracking";
 }
