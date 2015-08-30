c14a3da061805805b7b3ad04ed4e33a83c5db5e4
Harman Singh <harman.singh2@hp.com>
2015-08-30 11:49:27 +0530

    latest changes

--- app/src/main/java/org/android10/viewgroupperformance/activity/ProfileActivity.java
+++ app/src/main/java/org/android10/viewgroupperformance/activity/ProfileActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.android10.viewgroupperformance.R;
 
-public class ProfileActivity extends ActionBarActivity {
+public class ProfileActivity extends ActionBarActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
+    boolean likeSelected, commentSelected, shareSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class ProfileActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        likeSelected = commentSelected = shareSelected = false;
         String name = getIntent().getStringExtra(FeedListAdapter.NAME);
         String data = getIntent().getStringExtra(FeedListAdapter.DATA);
         String time = getIntent().getStringExtra(FeedListAdapter.TIME);
@@ -38,6 +42,38 @@ public class ProfileActivity extends ActionBarActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.profile_image);
         imageView.setImageResource(photo);
+
+        TextView statusName = (TextView) findViewById(R.id.feed_name);
+        statusName.setText(name);
+
+        TextView statusTime = (TextView) findViewById(R.id.feed_time);
+        statusTime.setText(time);
+
+        CircleImageView image = (CircleImageView) findViewById(R.id.feed_image);
+        image.setImageResource(photo);
+
+        if (name.equals("Ashish Singhal") || name.equals("Harman Singh")) {
+            TextView feedData = (TextView) findViewById(R.id.feed_data);
+            feedData.setVisibility(View.GONE);
+            ImageView feedDataImage = (ImageView) findViewById(R.id.feed_data_image);
+            feedDataImage.setVisibility(View.VISIBLE);
+        } else {
+            TextView feedData = (TextView) findViewById(R.id.feed_data);
+            feedData.setVisibility(View.VISIBLE);
+            feedData.setText(data);
+            ImageView feedDataImage = (ImageView) findViewById(R.id.feed_data_image);
+            feedDataImage.setVisibility(View.GONE);
+        }
+
+        LinearLayout like = (LinearLayout) findViewById(R.id.feed_like);
+        like.setOnClickListener(this);
+
+        LinearLayout comment = (LinearLayout) findViewById(R.id.feed_comment);
+        comment.setOnClickListener(this);
+
+        LinearLayout share = (LinearLayout) findViewById(R.id.feed_share);
+        share.setOnClickListener(this);
+
     }
 
     @Override
@@ -50,4 +86,53 @@ public class ProfileActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.feed_like:
+                LinearLayout likeL = (LinearLayout) v.findViewById(R.id.feed_like);
+                TextView likeText = (TextView) likeL.findViewById(R.id.feed_like_text);
+                ImageView likeImage = (ImageView) likeL.findViewById(R.id.feed_like_img);
+                if (likeSelected) {
+                    likeSelected = false;
+                    likeImage.setImageResource(R.drawable.ic_action_thumb_up);
+                    likeText.setTextColor(getResources().getColor(R.color.medium_grey));
+                } else {
+                    likeSelected = true;
+                    likeImage.setImageResource(R.drawable.ic_action_thumb_up_blue);
+                    likeText.setTextColor(getResources().getColor(R.color.primaryLight));
+                }
+                break;
+            case R.id.feed_comment:
+                LinearLayout commentL = (LinearLayout) v.findViewById(R.id.feed_comment);
+                TextView commentText = (TextView) commentL.findViewById(R.id.feed_comment_text);
+                ImageView commentImage = (ImageView) commentL.findViewById(R.id.feed_comment_image);
+                if (commentSelected) {
+                    commentSelected = false;
+                    commentImage.setImageResource(R.drawable.ic_communication_messenger);
+                    commentText.setTextColor(getResources().getColor(R.color.medium_grey));
+                } else {
+                    commentSelected = true;
+                    commentImage.setImageResource(R.drawable.ic_communication_messenger_blue);
+                    commentText.setTextColor(getResources().getColor(R.color.primaryLight));
+                }
+                break;
+            case R.id.feed_share:
+                LinearLayout shareL = (LinearLayout) v.findViewById(R.id.feed_share);
+                TextView shareText = (TextView) shareL.findViewById(R.id.feed_share_text);
+                ImageView shareImage = (ImageView) shareL.findViewById(R.id.feed_share_image);
+                if (shareSelected) {
+                    shareSelected = false;
+                    shareImage.setImageResource(R.drawable.ic_social_share);
+                    shareText.setTextColor(getResources().getColor(R.color.medium_grey));
+                } else {
+                    shareSelected = true;
+                    shareImage.setImageResource(R.drawable.ic_social_share_blue);
+                    shareText.setTextColor(getResources().getColor(R.color.primaryLight));
+                }
+                break;
+        }
+
+    }
 }
